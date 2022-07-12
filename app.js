a74639478f890656507f86f934f7164ccb1c617a
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-11 21:28:59 -0400

    multiplicar.js was created

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-const fs = require('fs');
+const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-console.log('=============================');
-console.log(`====     Tabla del: 5    ====`)
-console.log('=============================')
+const base = 8;
 
-
-
-const base = 5;
-
-let salida = '';
-
-for (let i = 1; i <= 10; i++) {
-
-    salida += `${base} x ${i} = ${base * i}\n`;
-}
-
-console.log(salida);
-
-fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-    if (err) throw err;
-    console.log(`tabla-${base}.txt creada con exito`)
-});
-
+crearArchivo(base)
+    .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
+    .catch(err => console.log(err));
