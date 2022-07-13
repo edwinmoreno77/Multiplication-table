d0c7dc8f6102bc400ccaf5f3d1bfed5291bd9683
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-12 20:38:45 -0400

    app.js optimized

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs').argv;
+const { argv } = require('./config/yargs');
+
 
 console.clear();
 
+crearArchivo(argv.base, argv.listar)
+    .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
+    .catch(err => console.log(err));
+
+
+
+
+
+
+
+
+// ejemplo educativo para entender de donde provienen lodos argumentos
 
 // console.log(process.argv);
+// console.log(argv);
 
-
-// de forma educativa para entender de donde provienen lodos argumentos
+// console.log('base: yargs', argv.base);
 
 // const [, , arg3 = 'base=5'] = process.argv;
 // const [, base = 5] = arg3.split('=');
 
 // console.log(arg3);
 
-
-
-
-
-// const base = 8;
-
-// crearArchivo(base)
-//     .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
-//     .catch(err => console.log(err));
+// const base = 9;
