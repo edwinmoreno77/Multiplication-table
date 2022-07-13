feb988eb2d8177dd43a1cdea80b8857e76e8efe3
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-12 20:38:17 -0400

    multiplicar.js changed

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 
 
-const crearArchivo = async (base = 5) => {
+const crearArchivo = async (base = 5, listar = false) => {
 
     try {
 
-        console.log('=============================');
-        console.log('====    Tabla del:', base, '  ======')
-        console.log('=============================')
-
         let salida = '';
-
         for (let i = 1; i <= 10; i++) {
 
             salida += `${base} x ${i} = ${base * i}\n`;
         }
 
-        console.log(salida);
+        if (listar) {
+            console.log('=============================');
+            console.log('====    Tabla del:', base, '  ======');
+            console.log('=============================');
+            console.log(salida);
+        }
 
         fs.writeFileSync(`tabla-${base}.txt`, salida);
 
@@ -26,8 +26,6 @@ const crearArchivo = async (base = 5) => {
 
         throw err;
     }
-
-
 }
 
 module.exports = {
